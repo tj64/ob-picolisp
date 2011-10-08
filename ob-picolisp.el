f66d1703a430699f71958adfcd2f902b3a919dc4
Thorsten <tj@data-driven.de>
2011-10-08 04:43:16 +0200

    Stripped down to the bare essentials required for external evaluation with variables (thanks to Eric Schulte). Any writing to STDOUT inhibited when ':results value'.

--- ob-picolisp.el
+++ ob-picolisp.el
@@ -2,7 +2,7 @@
 
 ;; Copyright (C) Thorsten
 
-;; Author: Thorsten
+;; Author: Thorsten and Eric Schulte
 ;; Keywords: literate programming, reproducible research
 ;; Homepage: http://orgmode.org
 ;; Version: 0.01
@@ -52,137 +52,57 @@
 
 ;;; Code:
 (require 'ob)
-(require 'ob-ref)
-(require 'ob-comint)
 (require 'ob-eval)
-;; possibly require modes required for your language
-(require `picolisp)
-(require `inferior-picolisp)
 
 ;; optionally define a file extension for this language
 (add-to-list 'org-babel-tangle-lang-exts '("picolisp" . "l"))
 
-;; optionally declare default header arguments for this language
-(defvar org-babel-default-header-args:picolisp
-  '((:colnames . "no"))
-  "Default arguments for evaluating a picolisp source block.")
-
-(defvar org-babel-picolisp-eoe "org-babel-picolisp-eoe"
-  "String to indicate that evaluation has completed.")
-
-(defcustom org-babel-picolisp-cmd picolisp-program-name
+(defcustom org-babel-picolisp-cmd "pil"
   "Name of command used to evaluate picolisp blocks."
   :group 'org-babel
   :type 'string)
 
-
-;; This function expands the body of a source code block by doing
-;; things like prepending argument definitions to the body, it should
-;; be called by the `org-babel-execute:picolisp' function below.
-
 (defun org-babel-expand-body:picolisp (body params &optional processed-params)
   "Expand BODY according to PARAMS, return the expanded body."
-  (let* ((vars (mapcar #'cdr (org-babel-get-header params :var)))
-         (result-params (cdr (assoc :result-params params)))
-         (print-level nil) (print-length nil)
-         (body (if (> (length vars) 0)
-                   (concat "(prog (let ("
-                           (mapconcat
-                            (lambda (var)
-                              (format "%S '%S)"
-				      (print (car var))
-				      (print (cdr var))))
-                            vars "\n      ")
-                           " \n" body ") )")
-                 body)))
-    (if (or (member "code" result-params)
-            (member "pp" result-params))
-        (concat "(pp " body ")") body)))
-
-
-;; This is the main function which is called to evaluate a code
-;; block.
-;; The most common first step in this function is the expansion of the
-;; PARAMS argument using `org-babel-process-params'.
+  (let ((vars (mapcar #'cdr (org-babel-get-header params :var)))
+        (result-params (cdr (assoc :result-params params)))
+        (print-level nil) (print-length nil))
+    (if (> (length vars) 0)
+        (concat "(prog (let ("
+                (mapconcat
+                 (lambda (var)
+                   (format "%S '%S)"
+                           (print (car var))
+                           (print (cdr var))))
+                 vars "\n      ")
+                " \n" body ") )")
+      body)))
 
 (defun org-babel-execute:picolisp (body params)
-   "Execute a block of Picolisp code with org-babel.  This function is
+  "Execute a block of Picolisp code with org-babel.  This function is
  called by `org-babel-execute-src-block'"           
-   (message "executing Picolisp source code block")
-   (let* (
-	  ;; name of the session or "none"
-	  (session-name (cdr (assoc :session params)))
-	  ;; set the session if the session variable is non-nil
-	  (session (org-babel-picolisp-initiate-session session-name))
-	  ;; either OUTPUT or VALUE which should behave as described above
-	  (result-type (cdr (assoc :result-type params)))
-	  ;; expand the body with `org-babel-expand-body:picolisp'
-	  (full-body (org-babel-expand-body:picolisp
-		      body params))
-          
-     ;; for session based evaluation the functions defined in
-     ;; `org-babel-comint' will probably be helpful.
-
-       (read
-	(if (not (string= session-name "none"))
-	    ;; session evaluation
-	      (org-babel-comint-with-output
-		  (session (format "%S" org-babel-picolisp-eoe) t full-body)
-		(mapc
-		 (lambda (line)
-		   (insert (org-babel-chomp line)) (comint-send-input nil t))
-		 (list full-body (format "%S" org-babel-picolisp-eoe))))
-	  ;; external evaluation
-          (let ((script-file (org-babel-temp-file "picolisp-script-")))
-            (with-temp-file script-file
-              (insert
-               ;; return the value or the output
-               (if (string= result-type "value")
-                   (format "%s" full-body)
-                 full-body)))
-            (org-babel-eval
-             (format "%s %s" org-babel-picolisp-cmd
-                     (org-babel-process-file-name script-file)) "")))))))
-
-(defun org-babel-picolisp-initiate-session (&optional session)
-  "If there is not a current inferior-process-buffer in SESSION
-then create.  Return the initialized session."
-  (unless (string= session "none")
-    (let ((session-buffer (save-window-excursion
-                            (run-picolisp picolisp-program-name)
-                            (rename-buffer session)
-                            (current-buffer))))
-      (if (org-babel-comint-buffer-livep session-buffer)
-          (progn (sit-for .25) session-buffer)
-        (sit-for .5)
-        (org-babel-picolisp-initiate-session session)))))
-
-
-;; This function should be used to assign any variables in params in
-;; the context of the session environment.
-
-;; (defun org-babel-prep-session:picolisp (session var-lines)
-;;   "Prepare SESSION according to the header arguments specified in PARAMS."
-;;      (when session
-;;       (org-babel-comint-in-buffer session
-;;         (sit-for .5) (goto-char (point-max))
-;;         (mapc (lambda (var)
-;;                 (insert var) (comint-send-input nil t)
-;;                 (org-babel-comint-wait-for-output session)
-;;                 (sit-for .1) (goto-char (point-max))) var-lines)))
-;;     session)
-
-
-;; (defun org-babel-picolisp-var-to-picolisp (var)
-;;   "Convert an elisp var into a string of picolisp source code
-;; specifying a var of the same value."
-;;   (format "%S" var))
-
-;; (defun org-babel-picolisp-table-or-string (results)
-;;   "If the results look like a table, then convert them into an
-;; Emacs-lisp table, otherwise return the results as a string."
-;;   )
-
+  (message "executing Picolisp source code block")
+  (let ((result-type (cdr (assoc :result-type params)))
+        (full-body (org-babel-expand-body:picolisp body params))
+        (script-file (org-babel-temp-file "picolisp-script-")))
+    (with-temp-file script-file
+      (insert (concat (if (string= result-type "value")
+                          ;(format "(print %s)" full-body)
+			  (concat (format "(print (out \"/dev/null\" %s)"
+					  full-body) ")")
+                        full-body)
+                      "(bye)")))
+    ((lambda (result)
+       (if (and (not (member 'verbatim result-params))
+                (not (member 'scalar result-params))
+                (> (length result) 0))
+           (read result)
+         result))
+     (org-babel-eval
+      (format "%s %s"
+              org-babel-picolisp-cmd
+              (org-babel-process-file-name script-file))
+      ""))))
 
 (provide 'ob-picolisp)
 ;;; ob-picolisp.el ends here
